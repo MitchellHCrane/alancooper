31c40b2be2d5bd7d306d7409ee308157100810f5
MitchellHCrane <mitchellhcrane@gmail.com>
2024-09-25 13:50:36 -0600

    init

---
+++ src/Components/Footer.js
@@ -0,0 +1,46 @@
+import "../css/footer.css";
+import "../css/App.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-div">
+        <div className="footerGridItem">
+          <h3 className="footerH3">Contact</h3>
+          <p className="footerP">
+            Phone: <a href="tel:254-733-3008">(254)-733-3008</a>
+          </p>
+          <p className="footerP">Fax: (254)-613-6326</p>
+          <p className="footerP">
+            Email:{" "}
+            <a href="mailto:alan@firstclasshomemortgage.com">
+              alan@firstclasshomemortgage.com
+            </a>
+          </p>
+        </div>
+        <div className="footerGridItem">
+          <h3 className="footerH3">License Info</h3>
+          <p className="footerP">Alan Cooper (NMLS #1912420)</p>
+          <p className="footerP">
+            Regulated by State of Utah Division of Real Estate NMLS #1843
+          </p>
+        </div>
+        <div className="footerGridItem">
+          <h3 className="footerH3">Address</h3>
+          <p className="footerP">First Class Home Mortgage</p>
+          <p className="footerP">
+            <a
+              href="https://goo.gl/maps/o97dcDxGxFcAcRheA"
+              target="_blank"
+              rel="noreferrer"
+            >
+              PO BOX 1091, Hewitt, Texas, 76643
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Components/Profile.js
@@ -0,0 +1,37 @@
+import "../css/profile.css";
+import "../css/App.css";
+
+function Profile() {
+  return (
+    <div className="profile-background">
+      <div className="profile-div">
+        <div className="profile-grid">
+          <div className="columnPic">
+            <img
+              className="headProfilePic"
+              src="../images/alanHeadshot.jpg"
+              alt="Alan Cooper Loan Officer"
+            />
+          </div>
+          <div>
+            <h1 className="profileName">Alan Cooper</h1>
+            <h2 className="NMLS">Loan Officer (NMLS #1912420)</h2>
+            <p className="profileP">
+              Hello I&apos;m Alan, I help families obtain the American Dream,
+              one home at a time! I look forward to helping you along your home
+              buying experience. I&apos;ve provided a few useful links to get
+              you started.
+            </p>
+            <div className="buttonBlueDiv">
+              <a href="#getStarted" className="buttonBlue">
+                Get Started
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
